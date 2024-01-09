ce03169d3e7fcaa6ab808214424403782db6c7ce
danglhph30148 <danglhph30148@fpt.edu.vn>
2024-01-09 18:37:15 +0700

    project

---
+++ my-app/src/app/services/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, inject } from '@angular/core'; 
+import { HttpClient } from '@angular/common/http'; 
+import { Product } from '../types/Product';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  
+  apiUrl = 'https://fakestoreapi.com/products'; // khai bao apiUrl
+  http = inject(HttpClient); // inject bien http
+  constructor() {}
+
+  getProductList(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl); //axios.get(apiUrl)
+  }
+}
